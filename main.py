5206c37f1570c544ffd052c9c85a36c4ea43e1c1
Kent <kredfox4@gmail.com>
2020-05-15 22:31:12 +0000

    added a decoding library

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# ask user if they want to code or decode
+ # user if they want to code or decode
 # get user input
 # loop each letter and assign it a number with a dictionary library
 # assign spaces to a random number from 27 - 100
@@ -8,27 +8,32 @@
 import random
 
 def codeSentence(codeBook):
+  jointWords = []
   sentence = input("Please type or paste your input here and hit enter:\n")
   for letter in sentence:
     if letter.upper() in codeBook:
       print(codeBook.get(letter.upper()))
+      print(jointWords.append(letter))
     elif letter == " " or letter == "  ":
-      print(random.randint(27,101))
+      print(random.randint(27,99))
+      print(jointWords.append(letter))
+
     
 
 
-def decodeSentence(codebook):
+def decodeSentence(deCodebook):
   sentence = input("Please type or paste the code you want translated and hit enter:\n") 
   for number in sentence:
-    if number in codeBook:
-     print(codeBook.get(number))
+    print(number)
+    print(deCodebook.get(number))
+    
      #FIXME get this to decode by finding the key of the value
 
 
 
 codeBook = {
-  "A" : "1",
   "B" : "2",
+  "A" : "1",
   "C" : "3",
   "D" : "4",
   "E" : "5",
@@ -55,6 +60,35 @@ codeBook = {
   "Z" : "26"
 }
 
+deCodeBook = {
+  "1" : "A", 
+  "2" : "B",
+  "3" : "C",  
+  "4" : "D",  
+  "5" : "E",  
+  "6" : "F",
+  "7" : "G",  
+  "8" : "H",  
+  "9" : "I",  
+  "10" : "J",
+  "11" : "K",
+  "12" : "L",
+  "13" : "M",
+  "14" : "N",
+  "15" : "O",
+  "16" : "P",
+  "17" : "Q",
+  "18" : "R",
+  "19" : "S",
+  "20" : "T",
+  "21" : "U",
+  "22" : "V",
+  "23" : "W",
+  "24" : "X",
+  "25" : "Y",
+  "26" : "Z"  
+}
+
 repeat = True
 
 while repeat == True:
@@ -74,4 +108,4 @@ if status == "1":
   codeSentence(codeBook)
 
 elif status == "2":
-  decodeSentence(codeBook)
+  decodeSentence(deCodeBook)
